87a5cf463a9770c29f57e5185d2270d32c74ccfc
isaacmirandaifce <53238295+isaacmirandaifce@users.noreply.github.com>
2023-09-19 14:43:33 -0300

    Add files via upload
    Arquivos com o modelo da estrutura básico da biblioteca

---
+++ aulaVetores/bibliotecaVetores.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <caminho_para_arquivo\bibliotecaVetoree.h>
+
+/// @brief Função que realiza a travessia do vetor, imprimindo cada uma de seus elementos e o tamanho do vetor.
+/// @param vetor 
+/// @param tamanhoAtualVetor 
+void travessiaVetor(int vetor[], int tamanhoAtualVetor){
+
+}
+
+/// @brief Função que executa uma busca linear dentro de um vetor
+/// @param vetor 
+/// @param tamanhoAtualVetor 
+/// @param valorElemento 
+/// @return retorna o indice do primeiro elemento identificado no array ou retorna -1 caso o elemento não esteja presente
+int buscaElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
+
+}
+
+/// @brief Funçção que insere um elemento numa posição especifica do vetor
+/// @param vetor 
+/// @param tamanhoAtualVetor 
+/// @param valorElemento 
+/// @param posicaoInsercao 
+void insereElemento(int vetor[], int tamanhoAtualVetor, int valorElemento, int posicaoInsercao){
+
+}
+
+/// @brief A função que deleta um elemento de um vetor. Caso o elemento nao seja encontrado não executa nenhum operação no vetor.
+/// @param vetor 
+/// @param tamanhoAtualVetor 
+/// @param valorElemento 
+/// @return Retorna o tamanho atual do vetor
+int excluirElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
+
+}
