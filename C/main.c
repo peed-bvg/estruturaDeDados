2c4b78b16e51af1981d002b7a2321bd4bc4dbb58
Wesley00s <wesley300rodrigues@gmail.com>
2023-09-20 10:58:47 -0300

    atualização

--- C/main.c
+++ C/main.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
-#include </home/wesley/estruturaDeDados/C/vetores.h>
+#include </home/wesley/Git/estruturaDeDados/C/vetores.h>
 #define TAM 10
 
 int main()
 {
     int vetor[TAM] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int opcao;
+    int elemento;
+    int posicao;
+    int newTam = TAM;
 
-    travessia(vetor, TAM);
-    printf("\n");
+    do{
+        printf("Digite a opção desejada:\n1 - Travessia do vetor\n2 - Inserir elemento no vetor\n3 - Buscar elemento no vetor\n4 - Excluir elemento do vetor\n5 - Sair\n");
+        scanf("%d", &opcao);
 
-    inserirElemento(vetor, TAM, 99, 7);
-    travessia(vetor, TAM);
-    printf("\n");
+        switch(opcao)
+        {
+            case 1:
+                travessia(vetor, newTam);
+            break;
 
-    buscarElemento(vetor, TAM, 5);
-    printf("\n");
+            case 2:
+                printf("\nInforme o elemento que deseja inserir no vetor: ");
+                scanf("%d", &elemento);
+                printf("Informe a posição em que deseja inserir o elemento de 0 a %d: ", newTam - 1);
+                scanf("%d", &posicao);
+                inserirElemento(vetor, newTam, elemento, posicao);
+            break;
 
-    excluirElemento(vetor, TAM, 5);
-    printf("\n\n");
+            case 3:
+                printf("\nInforme o elemento que deseja buscar: ");
+                scanf("%d", &elemento);
+                buscarElemento(vetor, newTam, elemento);
+            break;
+
+            case 4:
+                printf("\nInforme o elemento que deseja excluir: ");
+                scanf("%d", &elemento);
+                excluirElemento(vetor, TAM, elemento);
+                newTam -= 1;
+            break;
+
+            case 5:
+                printf("\nEncerrando...\n\n");
+            break;
+
+            default:
+                printf("\nOpção inválida!\n\n");
+        }
+    } while (opcao != 5);
+
 
     return 0;
 }
--- C/vetores.h
+++ C/vetores.h
@@ -1,30 +1,51 @@
 /// Função que executa uma busca linear dentro de um vetor
 void travessia(int array[], int tamanhoArray)
 {
-    printf("Travessia do vetor: ");
+    printf("\nTravessia do vetor: ");
 
     for (int contador = 0; contador < tamanhoArray; contador++)
     {
         printf("%d ", array[contador]);
     }
+    printf("\n\n");
 }
 
 /// Função que insere elemento numa posição epecífica dentro de um vetor
 void inserirElemento(int array[], int tamanhoArray, int elemento, int posicao)
 {
-    array[posicao] = elemento;
+    if (posicao <= tamanhoArray && posicao >= 0)
+    {
+        array[posicao] = elemento;
+        printf("\nElemento %d inserido com sucesso na posição %d do vetor!\n\n", elemento, posicao);
+    }
+    else
+    {
+        printf("\n[ERROR] Posição inválida!\n\n");
+    }
 }
 
 /// Função que executa uma busca dentro de um vetor
 void buscarElemento(int array[], int tamanhoArray, int elemento)
 {
-    printf("Busca de elemento: ");
+    int verifcador = 0;
+    int index = 0;
+
     for (int contador = 0; contador < tamanhoArray; contador++)
     {
         if (elemento == array[contador])
         {
-            printf("Elemento %d, encontrado na posição %d.", elemento, contador);
+            verifcador = 1;
+            index = contador;
         }
+    }
+
+    if (verifcador)
+    {
+        printf("\nElemento %d, encontrado na posição %d.\n\n", elemento, index);
+    }
+    else
+    {
+        printf("\nElemento não encontrado!\n\n");
     }
 }
 
@@ -32,21 +53,27 @@ void buscarElemento(int array[], int tamanhoArray, int elemento)
 void excluirElemento(int array[], int tamanhoArray, int elemento)
 {
     int index = 0;
+    int verificador = 0;
 
     for (int contador = 0; contador < tamanhoArray; contador++)
     {
-        for (int contador = 0; contador < tamanhoArray; contador++)
+        if (elemento == array[contador])
         {
-            if (elemento == array[contador])
-            {
-                index = contador;
-            }
+            index = contador;
+            verificador = 1;
         }
     }
 
-    for (int contador = index; contador < tamanhoArray - 1; contador++)
+    if(verificador)
     {
-        array[contador] = array[contador + 1];
+        for (int contador = index; contador < tamanhoArray - 1; contador++)
+        {
+            array[contador] = array[contador + 1];
+        }
+        printf("\nElemento %d excluído com sucesso!\n\n", elemento);
     }
-    travessia(array, tamanhoArray - 1);
+    else
+    {
+        printf("\nElemento não escontrado!\n\n");
+    }
 }
