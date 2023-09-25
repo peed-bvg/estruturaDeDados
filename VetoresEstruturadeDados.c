ec5d3ea053e8b0fe52509bb15b32667cf769e5be
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2023-09-25 20:51:39 -0300

    Update VetoresEstruturadeDados.c

--- VetoresEstruturadeDados.c
+++ VetoresEstruturadeDados.c
@@ -5,7 +5,9 @@
 //se ele existir, imprime o elemento procurado e sua posição
 //caso contrário, ele irá imprimir o elemento como não encontrado
 int buscaElemento(int vetor[], int *tamanhoAtualVetor, int valorElemento){
-    
+
+    //elementoEncontrado passa a veler 1 quando o elemento é encontrado
+    //caso seja diferente, será imprimido que o elemento não foi localiz
     int elementoEncontrado = 0;
     
     for (int i = 0; i < *tamanhoAtualVetor; i++) {
@@ -25,6 +27,9 @@ int buscaElemento(int vetor[], int *tamanhoAtualVetor, int valorElemento){
 
 
 //função para excluir um elemento no array
+//percorre o array e faz a comparação para achar o número desejado 
+//quando acha é feito uma segunda travessia de posição igual do i, para remover esse número para o fim do array
+//ele é excluido e o vetor tem seu tamanho de elementos diminuido
 int excluirElemento(int vetor[], int *tamanhoAtualVetor, int valorElemento){
         
         for (int i = 0; i < *tamanhoAtualVetor; i++) {
@@ -74,11 +79,19 @@ void insereElemento(int vetor[], int *tamanhoAtualVetor, int valorElemento, int 
 
 int main(){
     
+    //recebera interação do usuário para iniciar ou não o programa
+    int bottom;
     
+    printf("Aperte 1 para iniciar ou 0 para encerrar o programa.\n");
+    scanf("%d", &bottom);
+
+    do{
+
 
     int vetor[] = {5, 7, 9, 10, 12, 16, 65, 32};
     
     // Sizeof calcula o tamanho do vetor a partir da divisão do seu tamanho com o tamanho de um elemento do vetor;
+    //irá imprimir o vetor atual
     int tamanhoAtualVetor = sizeof(vetor)  / sizeof(vetor[0]); 
 
         printf("\nElementos atuais do vetor:\n");
@@ -88,15 +101,14 @@ int main(){
                 printf("%d ", vetor[i]);
             }
 
-
-
-    while(1){
-
+    
     //elementoProcurado recebe o elemento procurado
-    int elementoProcurado;
+     int elementoProcurado;
     
     printf("\nDigite o elemento que deseja encontrar: ");
     scanf("%d", &elementoProcurado);
+    
+   
     
             //Chama a função de busca ao elemento, chamando suas variáveis e substituindo outras para se adequar a condição estabelecida
             buscaElemento(vetor, &tamanhoAtualVetor, elementoProcurado);
@@ -141,11 +153,9 @@ int main(){
             
 
 
-}
+}while(bottom != 0);
+    
     return 0;
 }
-
-
-
 
 
