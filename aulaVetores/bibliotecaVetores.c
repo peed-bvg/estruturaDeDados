ca6775a3e708819b0b64d0c2e1b755fcb7c9b5d4
GabrielGuerreir0 <136819754+GabrielGuerreir0@users.noreply.github.com>
2023-09-26 19:43:31 -0300

    Update bibliotecaVetores.c

--- aulaVetores/bibliotecaVetores.c
+++ aulaVetores/bibliotecaVetores.c
@@ -6,10 +6,12 @@
 /// @param tamanhoAtualVetor 
 void travessiaVetor(int vetor[], int tamanhoAtualVetor){
 
-    for(int i = 0; i<tamanhoAtualVetor;i++){
-        printf("%d ", vetor[i]);
+    for (int i = 0; i < tamanhoAtualVetor; i++)
+    {
+        printf("%d ",vetor[i]);
     }
-
+    
+    
 }
 
 /// @brief Função que executa uma busca linear dentro de um vetor
@@ -18,6 +20,12 @@ void travessiaVetor(int vetor[], int tamanhoAtualVetor){
 /// @param valorElemento 
 /// @return retorna o indice do primeiro elemento identificado no array ou retorna -1 caso o elemento não esteja presente
 int buscaElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
+    for(int i=0;i<tamanhoAtualVetor;i++){
+        if(vetor[i]==valorElemento){
+            return i;
+        }
+    }
+    return -1;
 
 }
 
@@ -26,7 +34,23 @@ int buscaElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
 /// @param tamanhoAtualVetor 
 /// @param valorElemento 
 /// @param posicaoInsercao 
-void insereElemento(int vetor[], int tamanhoAtualVetor, int valorElemento, int posicaoInsercao){
+void insereElemento(int vetor[], int tamanhoAtualVetor, int valorElemento, int posicaoInsercao) {
+    if (posicaoInsercao < 0 || posicaoInsercao > tamanhoAtualVetor) {
+        printf("Posição de inserção inválida.\n");
+        break;
+    }
+    int novoTamanho = tamanhoAtualVetor + 1;
+    int novoVetor[novoTamanho];
+    for (int i = 0; i < posicaoInsercao; i++) {
+        novoVetor[i] = vetor[i];
+    }
+    novoVetor[posicaoInsercao] = valorElemento;
+    for (int i = posicaoInsercao; i < tamanhoAtualVetor; i++) {
+        novoVetor[i + 1] = vetor[i];
+    }
+    for (int i = 0; i < novoTamanho; i++) {
+        vetor[i] = novoVetor[i];
+    }
 
 }
 
@@ -36,5 +60,19 @@ void insereElemento(int vetor[], int tamanhoAtualVetor, int valorElemento, int p
 /// @param valorElemento 
 /// @return Retorna o tamanho atual do vetor
 int excluirElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
+    int aux[tamanhoAtualVetor];
+    int NovoTamanho = 0;
+for (int i = 0; i < tamanhoAtualVetor; i++)
+{
+    if(vetor[i]!=valorElemento){
+        aux[NovoTamanho]=vetor[i];
+        NovoTamanho++;
+    }
+}
+for (int i = 0; i < tamanhoAtualVetor; i++)
+{
+    vetor[i]=aux[i];
+}
+return NovoTamanho;
 
 }
