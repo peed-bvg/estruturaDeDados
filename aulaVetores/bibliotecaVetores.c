f5ff4c4e0de0cf12eede3bd20f6a57d7f8e2c342
Micaele25 <132933562+Micaele25@users.noreply.github.com>
2023-10-03 16:01:45 -0300

    Update bibliotecaVetores.c

--- aulaVetores/bibliotecaVetores.c
+++ aulaVetores/bibliotecaVetores.c
@@ -6,11 +6,14 @@
 /// @param tamanhoAtualVetor 
 void travessiaVetor(int vetor[], int tamanhoAtualVetor){
 
-    for(int i = 0; i<tamanhoAtualVetor;i++){
+printf("Elementos do vetor: ");
+    for (int i = 0; i < tamanhoAtualVetor; i++) {
         printf("%d ", vetor[i]);
     }
-
+    printf("\nTamanho atual do vetor: %d\n\n", tamanhoAtualVetor);
 }
+
+
 
 /// @brief Função que executa uma busca linear dentro de um vetor
 /// @param vetor 
@@ -18,6 +21,14 @@ void travessiaVetor(int vetor[], int tamanhoAtualVetor){
 /// @param valorElemento 
 /// @return retorna o indice do primeiro elemento identificado no array ou retorna -1 caso o elemento não esteja presente
 int buscaElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
+    for (int i = 0; i < tamanhoAtualVetor; i++) {
+        if (vetor[i] == valorElemento) {
+            printf("O elemento %d foi encontrado na posição %d.\n\n", valorElemento, i);
+            return i; 
+        }
+    }
+    printf("O elemento %d não foi encontrado no vetor.\n\n", valorElemento);
+    return -1;
 
 }
 
@@ -27,6 +38,26 @@ int buscaElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
 /// @param valorElemento 
 /// @param posicaoInsercao 
 void insereElemento(int vetor[], int tamanhoAtualVetor, int valorElemento, int posicaoInsercao){
+    id insereElemento(int vetor[], int *tamanhoAtualVetor, int valorElemento, int posicaoInsercao) {
+    if (*tamanhoAtualVetor < 6) {
+        // Verifica se a posição de inserção é válida
+        if (posicaoInsercao >= 0 && posicaoInsercao <= *tamanhoAtualVetor) {
+            // Desloca os elementos para abrir espaço para o novo elemento
+            for (int i = *tamanhoAtualVetor; i > posicaoInsercao; i--) {
+                vetor[i] = vetor[i - 1];
+            }
+            // Insere o novo elemento na posição especificada
+            vetor[posicaoInsercao] = valorElemento;
+            (*tamanhoAtualVetor)++;
+            printf("Elemento %d inserido com sucesso!\n\n", valorElemento);
+        } else {
+            printf("A posição de inserção não é válida.\n\n");
+        }
+    } else {
+        printf("O vetor está cheio, não é possível inserir mais elementos.\n\n");
+    }
+}
+
 
 }
 
@@ -36,5 +67,47 @@ void insereElemento(int vetor[], int tamanhoAtualVetor, int valorElemento, int p
 /// @param valorElemento 
 /// @return Retorna o tamanho atual do vetor
 int excluirElemento(int vetor[], int tamanhoAtualVetor, int valorElemento){
+      int encontrado = 0;
 
+    for (int i = 0; i < *tamanhoAtualVetor; i++) {
+        if (vetor[i] == valorElemento) {
+            encontrado = 1;
+
+            // Desloca os elementos à direita do elemento a ser excluído para preencher o espaço.
+            for (int j = i; j < *tamanhoAtualVetor - 1; j++) {
+                vetor[j] = vetor[j + 1];
+            }
+
+            (*tamanhoAtualVetor)--;
+            printf("Elemento %d foi excluído com sucesso!\n\n", valorElemento);
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        printf("O elemento %d não foi encontrado no vetor.\n\n", valorElemento);
+    }
+
+    return *tamanhoAtualVetor;
+}
+
+int main() {
+    int Vetor[6] = {1, 2, 3, 4, 5}; 
+    int tamanho = 5;
+
+    travessiaVetor(Vetor, tamanho);
+
+    int elementoBuscado = 3;
+    int indiceEncontrado = buscaElemento(Vetor, tamanho, elementoBuscado);
+
+    int elementoInserir = 6;
+    int posicaoInsercao = 2;
+    insereElemento(Vetor, &tamanho, elementoInserir, posicaoInsercao);
+
+    int elementoExcluir = 2;
+    tamanho = excluirElemento(Vetor, &tamanho, elementoExcluir);
+
+    travessiaVetor(Vetor, tamanho);
+
+    return 0;
 }
